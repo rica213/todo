b7a7625993728adb58cda8fe1fec8bce3432311e
rica213 <25f5k252l@mozmail.com>
2023-02-28 22:06:13 +0300

    Add routing with React router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import '@/styles/app.css';
-
+import "@/styles/app.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import TodoApp from "@/components/TodoApp";
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <Router>
+      <TodoApp />
+    </Router>
   </React.StrictMode>
 );
